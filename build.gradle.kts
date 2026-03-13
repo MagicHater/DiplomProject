e87a617255c8c0047e0254e4da6f2f67b85e7ed2
MagicHater <139489701+MagicHater@users.noreply.github.com>
2026-03-13 20:49:54 +0300

    Configure Kotlin JVM/Spring/JPA plugins at root for backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kotlin.jpa) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt.android) apply false
