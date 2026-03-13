106e9bf2babc2908774e2ab672e90693fd5b14af
MagicHater <139489701+MagicHater@users.noreply.github.com>
2026-03-13 23:49:41 +0300

    Fix backend standalone Gradle plugin resolution

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,1 +1,19 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        kotlin("jvm") version "1.9.24"
+        kotlin("plugin.spring") version "1.9.24"
+        kotlin("plugin.jpa") version "1.9.24"
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "adaptive-testing-backend"
